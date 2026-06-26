d2279e7310ced32b3f3c94c47f531f64259e9874
Vihaan9319 <ietankur.android@gmail.com>
2026-06-26 10:17:47 -0700

    Add files via upload

---
+++ weatherchecker.py
@@ -0,0 +1,5 @@
+t = float(input("What is the temperature today(in degrees celcius)? "))
+if t <= 20:
+    print("The weather is not suitable for wearing light clothes")
+else:
+    print("The weather is great for wearing light clothes.")
