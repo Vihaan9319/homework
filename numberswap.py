57b0962e7286c92c9665ba57ea5f36a614f81a1d
Vihaan9319 <ietankur.android@gmail.com>
2026-07-07 21:13:34 -0700

    Add files via upload

---
+++ numberswap.py
@@ -0,0 +1,10 @@
+a = 9
+b = 10
+c = 5
+print("Before swap, a is 9, b is 10, and c is 5")
+#The swapping part is now going to start
+temp = a
+a = b
+b = c
+c = temp
+print(f"After swap, a is {a}, b is {b}, and c is {c}")
