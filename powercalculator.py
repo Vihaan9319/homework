f361c94bf8997bd3dcd39f071373f29bc46a274b
Vihaan9319 <ietankur.android@gmail.com>
2026-07-16 14:12:51 -0700

    Add files via upload

---
+++ powercalculator.py
@@ -0,0 +1,6 @@
+print("Welcome to power calculator")
+n = float(input("Please enter a number to find the power of"))
+print("What number do you want placed as a power on", n)
+p = int(input("Enter the number"))
+s = n**p
+print("The answer is", s)
